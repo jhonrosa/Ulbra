9f64148a8a26fc2ef01fa7440ba576d287bd6ba4
Jhon <jhon.rosa@rede.ulbra.br>
2020-03-26 15:54:01 -0300

    att

--- Tarefas Algoritmos E Prog/VSCODE/C/Exercicios 25.03/Exercicio 5.c
+++ Tarefas Algoritmos E Prog/VSCODE/C/Exercicios 25.03/Exercicio 5.c
@@ -5,21 +5,21 @@
 
 #include <stdio.h>
 int main(){
-    int cd, idade;
-    char sexo;
-    printf("Digite seu código: ");
-    scanf("%i", &cd);
+    int idade;
+    char sexo, nome;
+    printf("Digite seu nome: ");
+    scanf("%s", &nome);
     printf("Digite sua idade: ");
     scanf("%i%*c", &idade);
     printf("Insira o seu sexo: M - masculino F - feminino: ");
     scanf("%c", &sexo);
 
     if (sexo == 'f' && idade<=25)
-        printf("Você está: ACEITA!");
+        printf("%s. Você está: ACEITA!");
    
     else
         {
-        printf("Você foi: RECUSADA!");
+        printf("%s. Você foi: RECUSADA!");
     }
     return 0;
 }
